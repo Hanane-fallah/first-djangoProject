0db8cedbed4d21475e606d5fd74281933fc4bd25
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-07 14:57:34 +0330

    001 - add funky view

--- views/urls.py
+++ views/urls.py
@@ -3,3 +3,7 @@ from . import views
 from django.views.generic import TemplateView
 
 app_name = 'views'
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='views/main.html')),
+    path('funky', views.funky)
+]
--- views/views.py
+++ views/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def funky(request):
+    response = """
+    <h1> funky function sample </h1>
+    """
+    return HttpResponse(response)
